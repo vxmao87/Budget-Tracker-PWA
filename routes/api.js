324d083dcfd2fb5d51dd0eb9e584ef70165fbded
Vinh Mao <vinhxmao87@gmail.com>
2020-07-18 12:05:52 -0700

    HTML route added, server updated

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
+router.get("/", function(req, res) {
+  res.json(path.join(__dirname, "../public/index.html"));
+})
+
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
